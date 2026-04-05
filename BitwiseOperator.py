551d71fb8f4323f43294bb9114972cf759d21a76
mikhailtsygankov2013-sudo <mikhailtsygankov2013@gmail.com>
2026-04-05 19:25:58 +0200

    Add files via upload

---
+++ BitwiseOperator.py
@@ -0,0 +1,9 @@
+a=10
+b=8
+
+print(a&b)
+print(~a)
+print(a>>b)
+print(a<<b)
+print(a^b)
+
---
+++ IdentityOperator.py
@@ -0,0 +1,13 @@
+a=[1,2,3,4,5]
+b=[1,2,3,4,5]
+print(a is b)
+
+c=a
+print(c is a)
+
+d=5
+e=5
+print(d is e)
+
+a="Hello my name is Michael"
+print("Michael" in a)
