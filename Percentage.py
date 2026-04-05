ef3632044a0df498e35136fa4fede6361e97a210
mikhailtsygankov2013-sudo <mikhailtsygankov2013@gmail.com>
2026-04-05 19:26:25 +0200

    Add files via upload

--- Percentage.py
+++ Percentage.py
@@ -10,3 +10,14 @@ print("Total marks out of 400:",total)
 
 p=(total/400)*100
 print("Percentage =",p)
+
+if p>90:
+    print("A Grade")
+elif p>80:
+    print("B Grade")
+elif p>70:
+    print("C Grade")
+elif p>60:
+    print("D Grade")
+else:
+    print("You failed")
