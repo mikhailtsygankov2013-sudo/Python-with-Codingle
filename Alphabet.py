8ab1a4734f7879594d188090e6fb8f0a4821a822
mikhailtsygankov2013-sudo <mikhailtsygankov2013@gmail.com>
2026-04-08 17:31:16 +0200

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,7 @@
+alph="abcdefghijklmnopqrstuvwxyz"
+letter=str(input("Enter a character")).lower()
+
+if letter in alph:
+    print("Character is in the alphabet!")
+else:
+    print("Character is not in the alphabet.")
